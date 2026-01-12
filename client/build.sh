6651c4d89079d3b8cf1c108c8d5c12858d26b041
Debdaru Dasgupta <debdarudasgupta0799@gmail.com>
2026-01-13 02:37:38 +0530

    updated vercel settings

--- client/build.sh
+++ client/build.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
-
-# Exit on error
 set -e
 
-# Enable Flutter Web
 flutter config --enable-web
-
-# Get packages
 flutter pub get
-
-# Build Flutter Web
 flutter build web --release

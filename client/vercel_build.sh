782de27ff55babf3aa1394d6fda06caedd9ba76a
Debdaru Dasgupta <debdarudasgupta0799@gmail.com>
2026-01-18 00:56:50 +0530

    updated vercel build file

--- client/vercel_build.sh
+++ client/vercel_build.sh
@@ -6,10 +6,8 @@ git clone https://github.com/flutter/flutter.git -b stable --depth 1
 
 export PATH="$PATH:`pwd`/flutter/bin"
 
-flutter doctor
 flutter config --enable-web
-
 flutter pub get
 
-# 🔥 FORCE HTML RENDERER
-flutter build web --release --web-renderer html
+# ✅ CORRECT FOR FLUTTER 3.38+
+flutter build web --release --renderer html

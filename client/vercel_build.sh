8c2401e434abd473335a8e26dd921d46765c21e2
Debdaru Dasgupta <debdarudasgupta0799@gmail.com>
2026-01-18 00:48:48 +0530

    vercel ci cd setup

---
+++ client/vercel_build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+echo "Installing Flutter SDK"
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
+
+export PATH="$PATH:`pwd`/flutter/bin"
+
+flutter doctor
+flutter config --enable-web
+flutter pub get
+flutter build web --release

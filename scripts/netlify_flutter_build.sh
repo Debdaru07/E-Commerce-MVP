7976d583c7128f0e8578d52aed19a28a25239041
Debdaru Dasgupta <debdarudasgupta0799@gmail.com>
2026-01-18 02:02:52 +0530

    updated netlify script

--- scripts/netlify_flutter_build.sh
+++ scripts/netlify_flutter_build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
 
-echo "🔧 Installing Flutter SDK"
-git clone https://github.com/flutter/flutter.git -b stable --depth 1
+echo "🔧 Setting up Flutter SDK"
 
-# Netlify base directory = client
+if [ ! -d "flutter" ]; then
+  echo "📥 Cloning Flutter SDK"
+  git clone https://github.com/flutter/flutter.git -b stable --depth 1
+else
+  echo "✅ Flutter SDK already exists"
+fi
+
 export PATH="$PATH:$(pwd)/flutter/bin"
 
 flutter config --enable-web

16a583131214932b5b898019ed1c5e90a061bbfc
Debdaru Dasgupta <debdarudasgupta0799@gmail.com>
2026-01-18 01:00:47 +0530

    vercel build update

--- client/vercel_build.sh
+++ client/vercel_build.sh
@@ -10,4 +10,5 @@ flutter config --enable-web
 flutter pub get
 
 # ✅ CORRECT FOR FLUTTER 3.38+
-flutter build web --release --renderer html
+flutter build web --release \
+  --dart-define=FLUTTER_WEB_RENDERER=html

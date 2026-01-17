a6b5dc251f772f1cd67f989f5454b234c9c08da7
Debdaru Dasgupta <debdarudasgupta0799@gmail.com>
2026-01-18 01:07:40 +0530

    updated build configurations.

--- client/lib/core/config/env.dart
+++ client/lib/core/config/env.dart
@@ -1,13 +1,28 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Env {
-  static String get baseUrl => dotenv.env['base_url']!;
+  static String get baseUrl => kReleaseMode
+      ? const String.fromEnvironment('BASE_URL')
+      : dotenv.env['base_url']!;
 
-  // AUTH
-  static String get adminLogin => dotenv.env['ADMIN_LOGIN']!;
-  static String get dealerLogin => dotenv.env['DEALER_LOGIN']!;
-  static String get consumerLogin => dotenv.env['CONSUMER_LOGIN']!;
+  static String get adminLogin => kReleaseMode
+      ? const String.fromEnvironment('ADMIN_LOGIN')
+      : dotenv.env['ADMIN_LOGIN']!;
 
-  static String get dealerSignup => dotenv.env['DEALER_SIGNUP']!;
-  static String get consumerSignup => dotenv.env['CONSUMER_SIGNUP']!;
+  static String get dealerLogin => kReleaseMode
+      ? const String.fromEnvironment('DEALER_LOGIN')
+      : dotenv.env['DEALER_LOGIN']!;
+
+  static String get consumerLogin => kReleaseMode
+      ? const String.fromEnvironment('CONSUMER_LOGIN')
+      : dotenv.env['CONSUMER_LOGIN']!;
+
+  static String get dealerSignup => kReleaseMode
+      ? const String.fromEnvironment('DEALER_SIGNUP')
+      : dotenv.env['DEALER_SIGNUP']!;
+
+  static String get consumerSignup => kReleaseMode
+      ? const String.fromEnvironment('CONSUMER_SIGNUP')
+      : dotenv.env['CONSUMER_SIGNUP']!;
 }
--- client/vercel_build.sh
+++ client/vercel_build.sh
@@ -11,4 +11,11 @@ flutter pub get
 
 # ✅ CORRECT FOR FLUTTER 3.38+
 flutter build web --release \
-  --dart-define=FLUTTER_WEB_RENDERER=html
+  --dart-define=BASE_URL=$BASE_URL \
+  --dart-define=ADMIN_LOGIN=$ADMIN_LOGIN \
+  --dart-define=DEALER_LOGIN=$DEALER_LOGIN \
+  --dart-define=CONSUMER_LOGIN=$CONSUMER_LOGIN \
+  --dart-define=DEALER_SIGNUP=$DEALER_SIGNUP \
+  --dart-define=CONSUMER_SIGNUP=$CONSUMER_SIGNUP \
+  --dart-define=FLUTTER_WEB_RENDERER=html \
+  --no-wasm-dry-run
